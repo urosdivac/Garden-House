29ffee027b2c5921afe076c2190a4ab2e3ea6d9b
dejvid98 <davidsucur111@gmail.com>
2020-05-08 17:19:58 +0200

    Homepage styling

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,36 @@
 import styles from "./index.module.scss";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img src="/assets/default-monochrome.svg" className={styles.logo} />
-      <p className={styles.slogan}>Platform for managing nurseries</p>
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+      <img src="/assets/Logo.svg" className={styles.logo} />
 
-        // * {
-        //   box-sizing: border-box;
-        // }
-      `}</style>
+      <p className={styles.slogan}>Platform for managing nurseries</p>
+
+      <Link href="/" className={styles.hoverz}>
+        <a className={styles.register}>Get started</a>
+      </Link>
+
+      <style jsx global>
+        {`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: "Lato", sans-serif;
+          }
+        `}
+      </style>
+
+      <Head>
+        <title>Garden House</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
     </div>
   );
 }
