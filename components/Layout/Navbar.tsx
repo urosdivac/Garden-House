99693e26e13fbb28033d205a189cc0523776c41e
dejvid98 <davidsucur111@gmail.com>
2020-06-04 23:14:19 +0200

    Navigation update

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,22 +1,27 @@
 import {useState, useEffect} from 'react';
 import Cookies from 'js-cookie';
 import Link from 'next/link';
+import getToken from '../../src/getToken';
 const styles = require('./Navbar.module.scss');
 
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState<{isadmin: boolean} | undefined>();
   const handleLogout = () => {
     Cookies.remove('JWT');
     setIsLoggedIn(false);
   };
+
   useEffect(() => {
     const cookie = Cookies.get('JWT');
     if (cookie) setIsLoggedIn(true);
+    setToken(getToken());
   }, []);
+  
   return (
     <div className={styles.containerz}>
       {isLoggedIn ? (
-        <Link href="/dashboard">
+        <Link href={token.isadmin ? '/dashbaord' : '/nursery'}>
           <a className={styles.login}>Dashboard</a>
         </Link>
       ) : null}
