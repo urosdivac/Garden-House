2561fc98f1a644180bf2bc9dbb867a17d0c5bd06
dejvid98 <davidsucur111@gmail.com>
2020-06-08 01:05:39 +0200

    Orders v2

--- components/Dashboard/Order/Order.tsx
+++ components/Dashboard/Order/Order.tsx
@@ -1,11 +1,141 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import Heading from './Heading';
+import axios from 'axios';
+import getToken from '../../../src/getToken';
+import OrderItem from './OrderItem';
 const styles = require('./Order.module.scss');
 
+interface Token {
+  id?: number;
+  shortname?: string;
+}
+
 const Order = () => {
+  const [orders, setOrders] = useState([]);
+  const [token, setToken] = useState<Token | undefined>();
+  const [statusFilter, setStatusFilter] = useState('declined');
+  const [filteredData, setFilteredData] = useState<any[] | undefined>();
+
+  const changeStatusFilter = filter => {
+    setStatusFilter(filter);
+
+    if (filter === 'all') {
+      setFilteredData(orders);
+      return;
+    }
+
+    setFilteredData([]);
+
+    orders.forEach(item => {
+      if (item.status == filter)
+        setFilteredData(prevState => [...prevState, item]);
+    });
+  };
+
+  const getOrders = async () => {
+    let query: string;
+    if (token) {
+      if (!token.shortname) {
+        query = 'https://gardenhouse.tech/order/user';
+        const data = await axios.post(query, {id: token.id});
+        console.log(data);
+        setOrders(data.data.data);
+        setFilteredData(data.data.data);
+      } else {
+        query = 'https://gardenhouse.tech/order/';
+        const data = await axios.post(query, {name: token.shortname});
+        setOrders(data.data.data);
+        setFilteredData(data.data.data);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!token) setToken(getToken());
+    getOrders();
+  }, [token]);
+
   return (
     <div className={styles.container}>
       <Heading />
+      <div className={styles.fields}>
+        <div
+          className={styles.fieldContainer}
+          onClick={() => {
+            changeStatusFilter('all');
+          }}
+        >
+          <div className={styles.all}>
+            <p>All</p>
+          </div>
+        </div>
+        <div
+          className={styles.fieldContainer}
+          onClick={() => {
+            changeStatusFilter('pending');
+          }}
+        >
+          <div className={styles.pending}>
+            <p>Pending</p>
+          </div>
+        </div>
+        <div
+          className={styles.fieldContainer}
+          onClick={() => {
+            changeStatusFilter('accepted');
+          }}
+        >
+          <div className={styles.accepted}>
+            <p>Accepted</p>
+          </div>
+        </div>
+        <div
+          className={styles.fieldContainer}
+          onClick={() => {
+            changeStatusFilter('declined');
+          }}
+        >
+          <div className={styles.declined}>
+            <p>Declined</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.labelsContainer}>
+        <div>
+          <p>ID</p>
+        </div>
+
+        <div>
+          <p>Order date</p>
+        </div>
+
+        <div>
+          <p>Delivery Date</p>
+        </div>
+
+        <div>
+          <p>Status</p>
+        </div>
+
+        <div>
+          <p>Functions</p>
+        </div>
+      </div>
+      <div className={styles.ordersContainer}>
+        {filteredData
+          ? filteredData.map(order => {
+              return (
+                <OrderItem
+                  id={order.id}
+                  status={order.status}
+                  deliverydate={order.delivery_date}
+                  orderdate={order.created_at}
+                  iscompany={token.shortname}
+                />
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 };
--- components/Dashboard/Order/OrderItem.tsx
+++ components/Dashboard/Order/OrderItem.tsx
@@ -1,11 +1,59 @@
-const styles = require('./OrderItem.module.scss')
+import moment from 'moment';
+import CheckIcon from '@material-ui/icons/Check';
+import BlockIcon from '@material-ui/icons/Block';
+const styles = require('./OrderItem.module.scss');
 
-const OrderItem = () => {
-    return (
-        <div className={styles.container}>
-            <h1>Item</h1>
-        </div>
-    )
+interface Props {
+  id: number;
+  orderdate: Date;
+  deliverydate?: Date;
+  status: string;
+  iscompany: string;
 }
 
-export default OrderItem
+const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+const OrderItem = ({id, orderdate, deliverydate, status, iscompany}: Props) => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <p>{id}</p>
+      </div>
+
+      <div>
+        <p>{moment(orderdate).format('MMMM Do YYYY, h:mm:ss a')}</p>
+      </div>
+
+      <div>
+        <p>
+          {deliverydate ? (
+            moment(deliverydate).format('MMMM Do YYYY, h:mm:ss a')
+          ) : (
+            <p>/</p>
+          )}
+        </p>
+      </div>
+
+      <div>
+        <p>{capitalizeFirstLetter(status)}</p>
+      </div>
+
+      <div>
+        {iscompany ? (
+          <span>
+            <CheckIcon className={styles.accept} />
+            <BlockIcon className={styles.decline} />
+          </span>
+        ) : !iscompany && status === 'pending' ? (
+          <BlockIcon className={styles.decline} />
+        ) : (
+          <p>/</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default OrderItem;
